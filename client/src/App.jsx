ad1fafa769f0488ca428abd4650d1101e58c3257
canjey <carolnjeri3235@gmail.com>
2024-09-23 06:55:09 -0700

    Create pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home';
+import SignUp from './Pages/SignUp';
+import Signin from './Pages/Signin';
+import Dashboard from './Pages/Dashboard';
+import About from './Pages/About';
 
 export default function App() {
   return (
-    <div>
-      <h1>Add</h1>
-      
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/about' element={<About />} />
+    <Route path='/dashboard' element={<Dashboard/>} />
+    <Route path='/signin' element={<Signin/>} />
+    <Route path='/signup' element={<SignUp/>} /> 
+
+   </Routes>
+   </BrowserRouter>
   )
 }
---
+++ client/src/Pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <div>
+                <h1>Sign Up</h1>
+
+      
+    </div>
+  )
+}
